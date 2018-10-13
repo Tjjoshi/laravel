8db613437b61da304814811b4ff6b275f5bc1a0a
toral <tj244@njit.edu>
2018-10-13 16:41:15 -0400

    task: change in header.blade.php file

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -13,7 +13,7 @@
                         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/discover-online-courses">Discover</a>
+                        <a class="nav-link" href="/about">About</a>
                     </li>
                     <li class="nav-item">
 
@@ -32,10 +32,7 @@
                         <a class="nav-link" href="/get-started">Get Started</a>
                     </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                    <button class="btn btn btn-success my-2 my-sm-0" type="submit">Find Courses</button>
-                </form>
+
             </div>
         </div>
     </nav>
