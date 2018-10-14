5658759418acf09bdcaf9ef83d562a7fdfe76f9a
toral <tj244@njit.edu>
2018-10-14 13:24:41 -0400

    feature: design about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts.default')
 
 @section('content')
+    <div class="container" >
+    <div class="row">
+    <h1 style="color:red;"> Remember Everything </h1>
+    </div>
 
-    <h1>Hello! welcome to about page </h1>
+        <div class="row">
+            <div class="text-center">
+            <h2 style="color:blue;"> For everything you'll do , MyNotes is a workspace to get it done more manageably </h2>
+            </div>
+        </div>
 
-!@stop
+        <div class="row">
+            <div class="text-center">
+                <h2 style="color:green;"> It helps you easily collect and find everything that matters. </h2>
+            </div>
+        </div>
+
+        <div class="row">
+           <h2 style="margin-left: auto;margin-right: auto;"> <span class="label label-default">One workspace </span></h2>
+    </div>
+
+        <div class="row">
+            <button class="btn btn-default btn-lg" style="margin-left:auto; margin-right:auto;">start now</button>
+        </div>
+    </div>
+@stop
