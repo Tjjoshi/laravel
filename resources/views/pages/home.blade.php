4cb56e2cffc79db781b42afcc3053667f309b8d9
toral <tj244@njit.edu>
2018-10-14 13:24:09 -0400

    feature: design homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.default')
 
 @section('content')
+    <div class="container">
+    <div class="row">
+        <button type="button" class="btn btn-primary btn-lg" >sign up</button>
+        <button type="button" class="btn btn-secondary btn-lg" style="margin-left:30px;">Login</button>
+    </div>
+
+    <div class="row">
+        <h1 style="color:red;">Welcome to MyNotes </h1>
+    </div>
+
+        <div class="row">
+            <div class="text-center">
+            <h2 style="color:blue;"> MyNotes helps you collect and organize everything you need to live and work smarter </h2>
+        </div>
+        </div>
 
 
-    <button type="button" class="btn btn-primary btn-lg" >sign up</button>
-    <button type="button" class="btn btn-secondary btn-lg" style="margin-left:30px;">Login</button>
-
+    </div>
 
 
 
